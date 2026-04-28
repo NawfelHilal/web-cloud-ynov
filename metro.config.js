4f4dee03e84544140b139ee3ff3601a3877ca134
NawfelHilal <nawfelhilal13@gmail.com>
2026-04-28 11:36:15 +0200

    ci: add GitHub Actions workflow for web/android deployment and update metro configuration

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,9 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// On s'assure d'ajouter 'mjs' aux extensions sources
-const { transformer, resolver } = config;
-
-config.resolver.sourceExts = [...resolver.sourceExts, 'mjs'];
+config.resolver.sourceExts.push('mjs');
 
 module.exports = config;
