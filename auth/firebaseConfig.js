56287f7736eba93870101ee2ae54c9a6ee6556f9
NawfelHilal <nawfelhilal13@gmail.com>
2026-04-30 12:04:35 +0200

    feat: integrate Firebase configuration and add CI/CD workflow for web and Android deployment

--- auth/firebaseConfig.js
+++ auth/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "web-cloud-ynov-fee89.firebaseapp.com",
-  projectId: "web-cloud-ynov-fee89",
-  storageBucket: "web-cloud-ynov-fee89.firebasestorage.app",
-  messagingSenderId: "7831595404",
-  appId: "1:7831595404:web:895fc66a9eb730eba38ea9",
-  measurementId: "G-P2H74NZKRT"
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
